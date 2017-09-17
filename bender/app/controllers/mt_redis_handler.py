1de25a07054f6ad1ce3358ce22706df4f803a717
nvh-gel <nvhien2703@outlook.com>
2017-09-17 23:15:53 +0700

    correct supervisord config, add mt redis docstring

--- bender/app/controllers/mt_redis_handler.py
+++ bender/app/controllers/mt_redis_handler.py
@@ -1,8 +1,12 @@
+""" Handle redis connection """
 from models.mt_redis import MTRedis
+
 
 REDIS = None
 
+
 def get_redis():
+    """ Return redis connection """
     global REDIS
     REDIS = MTRedis()
     return REDIS.get_redis()
