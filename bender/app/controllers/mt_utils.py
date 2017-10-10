ff10d4e7b1af292f45f61dbdaefd1d68c49a50ed
nvh-gel <nvhien2703>
2017-10-10 23:12:55 +0700

    update classification from new file

--- bender/app/controllers/mt_utils.py
+++ bender/app/controllers/mt_utils.py
@@ -1,6 +1,8 @@
 """ Convenient function """
 import re
 from app.controllers.mt_config_handler import MTConfigHandler
+from app.controllers.mt_io import print_stderr
+
 
 def is_jira_key(key):
     """ Check passed string is jira or not,
@@ -18,12 +20,19 @@ def classify_issue(message):
     """ Classify issue type, team, component
     accept alert message
     return dict of classification """
-    classifying_array = MTConfigHandler().get_config_section('classification')
-    for issuetype in classifying_array:
-        if next(iter(issuetype.keys())) in message.lower():
-            details_issue = next(iter(issuetype.values()))
-            for details_type in details_issue['details']:
-                if next(iter(details_type.keys())) in message.lower():
-                    return next(iter(details_type.values()))
-            return details_issue['default']
-    return {'project': 'BUG', 'issuetype': 'Bug', 'component': '911', 'priority': 'P1', 'team': ''}
+    classifying_dict = MTConfigHandler().get_config_section('classification')
+    return browse_classifying_dict(message.lower(), issue_dict=classifying_dict)
+
+
+def browse_classifying_dict(issue, issue_dict=None):
+    """ Keep browsing classfying tree
+    till no more details """
+    if 'details' not in issue_dict:
+        return issue_dict.get('default', {})
+    else:
+        for issue_details in issue_dict['details']:
+            issue_key = next(iter(issue_details.keys()))
+            if issue_key in issue:
+                print_stderr(issue_key)
+                return browse_classifying_dict(issue, issue_dict=issue_details[issue_key])
+        return issue_dict.get('default', {})
